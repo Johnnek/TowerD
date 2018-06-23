6f40070e5a0a8c725c88b1e7941a6123e73fc9b7
smashing <smashing@SMASHING-PC>
2018-06-23 13:45:22 +0200

    yo

--- TD/TD/Main.cpp
+++ TD/TD/Main.cpp
@@ -1,4 +1,24 @@
-void main() 
-{
+#include <iostream>
+#include <Windows.h>
+#ifdef WIN32
+#undef main
+#pragma comment(lib, "SDL2.lib")
+#pragma comment(lib, "SDL2main.lib")
+#pragma comment(lib, "SDL2_image.lib")
+#pragma comment(lib, "SHELL32.LIB" )
+#pragma comment( lib, "Winmm.lib" )
+#pragma comment(lib, "urlmon.lib")
+#include <SDL.h>
+#include <SDL_image.h>
+#else
+#include <SDL/SDL.h>
+#endif
+#ifdef _WIN32
+#undef main
+#endif 
 
+int main() 
+{
+	//
+	return 0;
 }
